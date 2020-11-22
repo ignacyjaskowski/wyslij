526a034472d043aa60994d804a245adc0916495d
ignacyjaskowski <74858286+ignacyjaskowski@users.noreply.github.com>
2020-11-22 11:45:36 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,26 @@
+let a = 0
+let n = 0
+input.onPinPressed(TouchPin.P0, function () {
+    radio.sendNumber(1)
+    a = a - 1
+    basic.showNumber(a)
+})
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(0)
+    n = n + 1
+    basic.showNumber(n)
+})
+input.onPinPressed(TouchPin.P2, function () {
+    radio.sendNumber(2)
+    basic.showNumber(0)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendString("fajf")
+    n = n - 1
+    basic.showNumber(n)
+})
+input.onPinPressed(TouchPin.P1, function () {
+    radio.sendString("fajffajf")
+    a = a + 1
+    basic.showNumber(a)
+})
